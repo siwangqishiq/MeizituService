63f20c90ac6ab2f7674c9a7ef02f4e09c6278438
yixin.panyi <panyi@yixin.im>
2020-05-27 11:09:51 +0800

    Initial commit

---
+++ src/main/java/panyi/xyz/meizitu/MainApplication.java
@@ -0,0 +1,11 @@
+package panyi.xyz.meizitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}//end class
